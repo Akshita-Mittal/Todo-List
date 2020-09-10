a41ab6f3b1a060347082a85bc9a399c08b550e3d
Akshita mittal <you@example.com>
2020-09-10 15:48:08 +0530

    Updates

---
+++ precache-manifest.12b751130f5a27eda01a9014471238f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3283fb71ede8ba0024588122c84771c9",
+    "url": "/Todo-List/index.html"
+  },
+  {
+    "revision": "a453b915a0276770299b",
+    "url": "/Todo-List/static/css/main.70c10899.chunk.css"
+  },
+  {
+    "revision": "7b7a2ea9d54fcd773124",
+    "url": "/Todo-List/static/js/2.ef0a374c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-List/static/js/2.ef0a374c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a453b915a0276770299b",
+    "url": "/Todo-List/static/js/main.cb63e4ec.chunk.js"
+  },
+  {
+    "revision": "f3d7905094aee1f38206",
+    "url": "/Todo-List/static/js/runtime-main.b54083ff.js"
+  }
+]);
